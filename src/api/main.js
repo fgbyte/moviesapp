c306c332df62b826cc6dd0d90be1b5eb39fd474d
fgbyte <fer.garciabayon@gmail.com>
2023-05-07 12:30:12 -0400

    switch to categories design

--- src/api/main.js
+++ src/api/main.js
@@ -2,21 +2,30 @@ import { API_KEY } from "./secrets"
 
 export async function getTrendingMoviesPreview() {
     //GET /trending/{media_type}/{time_window}
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
-    const data = await res.json()
-    //data === obj con las props "page" => number y "result" => [ 20 resultados ]
+   
+   try {
+       const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
+       const data = await res.json()
+       //data === obj con las props "page" => number y "result" => [ 20 resultados ]
 
-     const movies = data.results
-     return movies
-    //movies nos trae los objetos de 20 movies en tendencia
+        const movies = data.results
+        return movies
+       //movies nos trae los objetos de 20 movies en tendencia
+   } catch(error) {
+        return console.error(error)
+   }
 
 }
 
 export async function getPopularMovies() {
     // GET /movie/popular
-    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY)
-    const data = await res.json()
+    try {
+        const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY)
+        const data = await res.json()
 
-    const movies = data.results
-     return movies
+        const movies = data.results
+         return movies
+    } catch(error) {
+        return console.log(error)
+    }
 }
