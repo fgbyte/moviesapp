1d4a38f78898f31c29c6f62d448f6278f7e1dde6
fgbyte <fer.garciabayon@gmail.com>
2023-05-15 17:21:31 -0400

    redeploy test

--- src/api/redeploy.js
+++ src/api/redeploy.js
@@ -1,7 +1,15 @@
 const fetch = require('node-fetch');
 
-setInterval(() => {
-  fetch('https://api.vercel.com/v1/integrations/deploy/prj_HAllRRZAkZXUDneeXRizJbWZRps7/XmbYhilFPW?buildCache=false').then(() => {
-    console.log('Redeploy iniciado');
-  });
-}, 24 * 60 * 60 * 1000); // redeploy cada 24 horas
+function redeploy() {
+  fetch('https://api.vercel.com/v1/integrations/deploy/prj_HAllRRZAkZXUDneeXRizJbWZRps7/XmbYhilFPW?buildCache=false')
+    .then(() => {
+      console.log('Redeploy iniciado');
+    })
+    .catch((error) => {
+      console.error('Error al iniciar el redeploy:', error);
+    });
+}
+
+// setInterval(redeploy, 24 * 60 * 60 * 1000); // redeploy cada 24 horas
+
+setInterval(redeploy, 5 * 60 * 1000); // redeploy en 5 minutes
