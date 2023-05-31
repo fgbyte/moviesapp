43543183211c202cd5ac1e62c21be57d9b660107
fgbyte <fer.garciabayon@gmail.com>
2023-05-31 11:58:32 -0400

    testing vercel cro jobs 5min [CHANGE]

--- api/cron.js
+++ api/cron.js
@@ -1,11 +1,13 @@
-async function redeploy() {
-    const endpoint = 'https://api.vercel.com/v1/integrations/deploy/prj_HAllRRZAkZXUDneeXRizJbWZRps7/XmbYhilFPW?buildCache=false';
+export default function handler(req, res) {
+    const endpoint =
+        "https://api.vercel.com/v1/integrations/deploy/prj_HAllRRZAkZXUDneeXRizJbWZRps7/XmbYhilFPW?buildCache=false";
     try {
-      await fetch(endpoint);
-      console.log('Redeploy initiated');
+        fetch(endpoint);
+        console.log("Redeploy initiated");
+        res.status(200).end("Redeploy initiated");
     } catch (error) {
-      console.error('Error initiating redeploy:', error);
+        console.error("Error initiating redeploy:", error);
+        res.status(500).end("Error initiating redeploy");
     }
-  }
+}
 
-  redeploy()
